81aea9dc81186f2c87f6003803a5d4fd28c789e8
macstudent <macstudent@dhillss-imac-8.local>
2019-02-14 16:42:54 -0800

    Initial Commit

---
+++ tipapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipapp
+//
+//  Created by macstudent on 2/14/19.
+//  Copyright © 2019 macstudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
